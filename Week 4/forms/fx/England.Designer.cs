c459b4a56d153870669a0feed3ec07a1d75bfa5e
leminhtanvatc <leminhtanvatc@yahoo.com.vn>
2017-10-22 09:53:11 +0700

    Add files via upload

---
+++ Week 4/forms/fx/England.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace echo_main.forms.fx
+{
+    partial class England
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // England
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackgroundImage = global::echo_main.Properties.Resources.england;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(991, 468);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "England";
+            this.ShowInTaskbar = false;
+            this.Text = "England";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.England_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Week 4/forms/fx/England.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using echo_main.BS_layer.Func;
+using echo_main.res;
+
+namespace echo_main.forms.fx
+{
+    public partial class England : Form
+    {
+        public England()
+        {
+            InitializeComponent();
+        }
+
+        private async void England_Load(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+            Info.dct_data = await FuncDict.get_dict(Info.current_dir + @"\dictdata\2790517_Anh-Viet\Anh-Viet.dict");
+            Info.lst_music = await FuncCommon.get_music_files();
+            Persistent.FrmMenu.Show();
+            await opa_to_zero();
+            Hide();
+        }
+
+        private async Task opa_to_zero()
+        {
+            while (Opacity > 0)
+            {
+                Opacity-=0.01;
+                await Task.Delay(1);
+            }
+        }
+    }
+}
